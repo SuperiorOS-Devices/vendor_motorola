5eccd550097a6a95d7af53c2858010e26c5b2719
B4579 <B4579@protonmail.com>
2022-09-10 21:46:17 +0200

    dubai: Decommonize
    Signed-off-by: B4579 <B4579@protonmail.com>

--- nio/proprietary/vendor/radio/qcril_database/upgrade/35_version_qcrildb.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT,value TEXT, PRIMARY KEY(property));
-INSERT OR REPLACE INTO qcril_properties_table (property, value) VALUES ('qcrildb_version', 35);
-
-INSERT INTO "qcril_emergency_source_voice_table" VALUES('466','110','','full');
-INSERT INTO "qcril_emergency_source_voice_table" VALUES('466','119','','full');
-
-COMMIT TRANSACTION;
